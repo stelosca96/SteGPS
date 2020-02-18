fde278e7d27306d3773d571461bbb14da47a0e57
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-02-18 20:36:23 +0100

    GPS library fix

--- steGPS/gps.py
+++ steGPS/gps.py
@@ -2,33 +2,34 @@ import time
 import serial
 import re
 
-from steGPS.haversine import haversine
+from .haversine import haversine
 from threading import Thread
 from datetime import datetime, timedelta
 
 
 class Gps:
 
-    def __init__(self, serial_port, timezone_hours: int = 0, serial_baudrate: int = 9600):
-        self.serial_ = serial.Serial(serial_port, serial_baudrate)
+    def __init__(self, serial_port: str, timezone_hours: int = 0, serial_baudrate: int = 9600):
+        self.__serial = serial.Serial(serial_port, serial_baudrate)
         # self.file = open("aaa.txt", "a+")
-        self.timezone_offset = timedelta(hours=timezone_hours)
-        self.quality_ = 0  # 0 not fixed / 1 standard GPS / 2 differential GPS / 3 estimated (DR) fix
+        self.__timezone_offset = timedelta(hours=timezone_hours)
+        self.__quality = 0  # 0 not fixed / 1 standard GPS / 2 differential GPS / 3 estimated (DR) fix
 
-        self.speed_ = 0  # speed over ground km/h
-        self.altitude_ = 0  # altitude (m)
-        self.latitude_ = 0
-        self.longitude_ = 0
-        self.satellites_ = 0  # number of satellites in use
-        self.time_ = 0  # UTC time hhmmss.ss
-        self.date_ = 0  # date in day, month, year format ddmmyy es. 091219
-
+        self.__speed = 0  # speed over ground km/h
+        self.__altitude = 0  # altitude (m)
+        self.__latitude = 0
+        self.__travelled_distance = 0
+        self.__longitude = 0
+        self.__satellites = 0  # number of satellites in use
+        self.__time = 0  # UTC time hhmmss.ss
+        self.__date = 0  # date in day, month, year format ddmmyy es. 091219
+        self.__pos_0 = ('', '')
         self._worker = Thread(target=self._run, daemon=False)
         self._worker.start()
 
     @property
     def fixed(self):
-        return self.quality_ != 0
+        return self.__quality != 0
 
     @property
     def position(self):
@@ -37,39 +38,39 @@ class Gps:
 
     @property
     def altitude(self):
-        return self.altitude_
+        return self.__altitude
 
     @property
     def latitude(self):
-        return self.latitude_
+        return self.__latitude
 
     @property
     def longitude(self):
-        return self.longitude_
+        return self.__longitude
 
     @property
     def speed(self):
-        return self.speed_
+        return self.__speed
 
     @property
     def satellites(self):
-        return self.satellites_
+        return self.__satellites
 
     @property
     def time(self):
         # UTC time hhmmss.ss
-        p_hours = str(self.time_)[0:2]
-        p_minutes = str(self.time_)[2:4]
-        p_seconds = str(self.time_)[4:6]
+        p_hours = str(self.__time)[0:2]
+        p_minutes = str(self.__time)[2:4]
+        p_seconds = str(self.__time)[4:6]
         return '{}:{}:{}'.format(p_hours, p_minutes, p_seconds)
 
     @property
     def date(self):
         # date in day, month, year format ddmmyy es. 091219
-        self.date_ = "010100" if self.date_ == 0 else str(self.date_)
-        p_day = self.date_[0:2]
-        p_month = self.date_[2:4]
-        p_year = self.date_[4:6]
+        self.__date = "010100" if self.__date == 0 else str(self.__date)
+        p_day = self.__date[0:2]
+        p_month = self.__date[2:4]
+        p_year = self.__date[4:6]
         return '{}-{}-{}'.format(p_year, p_month, p_day)
 
     @property
@@ -85,14 +86,14 @@ class Gps:
 
     @property
     def travelled_distance(self):
-        return 0
+        return self.__travelled_distance
 
     def _run(self):
         last_print = time.monotonic()
         while True:
             try:
                 # serial read line by line
-                line = self.serial_.read_until('\n'.encode()).decode()
+                line = self.__serial.read_until('\n'.encode()).decode()
                 # print(line)
                 self.parse_line(line)
             except UnicodeDecodeError:
@@ -114,28 +115,47 @@ class Gps:
     def parse_xxGGA(self, line: list):
         if line.__len__() < 15:
             return
-        self.time_ = line[1]
-        self.latitude_ = line[2]
-        self.longitude_ = line[4]
-        self.quality_ = line[6]
-        self.satellites_ = line[7]
-        self.altitude_ = line[9]
+        self.__time = line[1]
+        if self.__latitude != '' and self.__longitude != '':
+            self.nmea_cord_to_decimal(line[3], line[5])
+            self.add_distance(self.latitude, self.longitude)
+        self.__quality = int(line[6])
+        self.__satellites = int(line[7])
+        self.__altitude = float(line[9])
 
     def parse_xxGLL(self, line):
         if line.__len__() < 8:
             return
-        self.latitude_ = line[1]
-        self.longitude_ = line[3]
+        if self.__latitude != '' and self.__longitude != '':
+            self.nmea_cord_to_decimal(line[1], line[3])
 
     def parse_xxRMC(self, line):
         if line.__len__() < 13:
             return
-        self.time_ = line[1]
-        self.latitude_ = line[3]
-        self.longitude_ = line[5]
-        self.date_ = line[9]
+        self.__time = line[1]
+        if self.__latitude != '' and self.__longitude != '':
+            self.nmea_cord_to_decimal(line[3], line[5])
+        self.__date = line[9]
 
     def parse_xxVTG(self, line):
         if line.__len__() < 10:
             return
-        self.speed_ = line[7]
+        self.__speed = line[7]
+
+    @travelled_distance.setter
+    def travelled_distance(self, value):
+        self.__travelled_distance = value
+
+    def add_distance(self, latitude, longitude):
+        pos_1 = (latitude, longitude)
+        if longitude != '' and latitude != '' and self.__pos_0[0] != '' and self.__pos_0[1] != '':
+            d = haversine(self.__pos_0, pos_1)
+            if d > 1:
+                self.__travelled_distance += d
+        self.__pos_0 = pos_1
+
+    def nmea_cord_to_decimal(self, latitude, longitude):
+        if not re.match("[0-9]{4}.[0-9]+", latitude) or not re.match("[0-9]{5}.[0-9]+", longitude):
+            return
+        self.__latitude = float(latitude[0:2]) + float(latitude[2:])/60
+        self.__longitude = float(longitude[0:3]) + float(longitude[3:])/60
